b3d65ed98bae1713e16e83c884e36c7acf7fcb0a
brookewil <bwilson4@ithaca.edu>
2023-10-31 14:10:47 -0400

    Database Models

--- app/models.py
+++ app/models.py
@@ -8,3 +8,11 @@ class Users(db.Model):
     name = db.Column(db.String(64), index=True, unique=True)
     bio = db.Column(db.String(256), index=True)
 
+class Quest(db.Model):
+    id = db.Column(db.integer, primary_key=True, autoincrement=True)
+    description = db.Column(db.Text, index=True, nullable=False)
+    image = db.Column(db.LargeBinary)
+    createdby = db.relationship("Users", backref="user")
+
+    #user_id = db.Column(db.integer, db.ForeignKey("user.id"))
+
