a53ca6cd5012008630912b18a0377c923017c9dd
lukepotter <lpotter1@ithaca.edu>
2023-10-31 14:11:25 -0400

    I set up the database

---
+++ app/forms.py
@@ -0,0 +1,37 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, TextAreaField, DateField, SelectField, SelectMultipleField, BooleanField, \
+    PasswordField
+from wtforms.validators import DataRequired, ValidationError, Optional, Email, EqualTo
+
+from app.models import *
+
+
+class NewUserForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Create Account')
+
+    def validate_username(self, username):
+        user = Users.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different username.')
+
+    def validate_email(self, email):
+        user = Users.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different email address.')
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Login')
+
+
+class CreateQuestForm(FlaskForm):
+    quest_name = StringField('SideQuest Name', validators=[DataRequired])
+    description = StringField('Description', validators=[DataRequired])
+    submit = SubmitField('Create')
--- app/models.py
+++ app/models.py
@@ -6,5 +6,20 @@ from app import db, login
 class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), index=True, unique=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
     bio = db.Column(db.String(256), index=True)
+    password_hash = db.Column(db.String(128))
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+
+
+
+
+
 
