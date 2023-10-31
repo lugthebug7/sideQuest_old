a1c41ed4402ffd5cd651372382eec837d63e54e8
brookewil <bwilson4@ithaca.edu>
2023-10-31 14:12:05 -0400

    Database Models

--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 from app import db, login
 
-class Users(db.Model):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), index=True, unique=True)
     username = db.Column(db.String(64), index=True, unique=True)
@@ -16,12 +16,6 @@ class Users(db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
-
-
-
-
-
-
 
 class Quest(db.Model):
     id = db.Column(db.integer, primary_key=True, autoincrement=True)
