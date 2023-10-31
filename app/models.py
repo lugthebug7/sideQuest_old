859ec0598afa8ee790697d0a149d739381b3a94f
brookewil <bwilson4@ithaca.edu>
2023-10-31 14:20:47 -0400

    Database Models

--- app/models.py
+++ app/models.py
@@ -4,10 +4,10 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from app import db, login
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(64), index=True, unique=True)
     username = db.Column(db.String(64), index=True, unique=True)
-    email = db.Column(db.String(120), index=True, unique=True)
+    email = db.Colum\n(db.String(120), index=True, unique=True)
     bio = db.Column(db.String(256), index=True)
     password_hash = db.Column(db.String(128))
 
@@ -25,3 +25,9 @@ class Quest(db.Model):
 
     #user_id = db.Column(db.integer, db.ForeignKey("user.id"))
 
+class Review(db.Model):
+    id = db.Column(db.integer, primary_key=True, autoincrement=True)
+    review_text = db.Column(db.Text, index=True)
+    image = db.Column(db.LargeBinary)
+    user_id = db.Column(db.integer, db.ForeignKey("user.id"))
+    quest_id = db.Column(db.integer, db.ForeignKey("quest.id"))
