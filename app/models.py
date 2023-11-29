e9ce46a1465d8dd4373cc9118bf97ceaece69e98
lukepotter <lpotter1@ithaca.edu>
2023-11-29 18:42:18 -0500

    Edits to profile page. Also, added profile pic to registration page

--- app/models.py
+++ app/models.py
@@ -11,6 +11,7 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     bio = db.Column(db.String(256), index=True)
     password_hash = db.Column(db.String(128))
+    image = db.Column(db.LargeBinary)
     admin = db.Column(db.Boolean, default=False)
 
     created_by = db.relationship("Quest", backref="user", lazy='dynamic')
